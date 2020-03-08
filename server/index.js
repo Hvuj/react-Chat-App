a1cba0b063172fda25f221dd200e22335c464c26
Eli Sigal <elis@hackerucrm.co.il>
2020-03-08 13:05:30 +0200

    react chat bot

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const {addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
 
@@ -12,7 +13,10 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
+app.use(cors());
+app.use(router);
+
+io.on('connect', (socket) => {
     socket.on('join', ({name, room}, callback)=>{
         const {error, user} = addUser({id: socket.id, name, room});
 
@@ -25,23 +29,30 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+    
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
         callback();
     });
 
     socket.on('sendMessage', (message, callback) => {
         const user = getUser(socket.id);
 
-        io.to(user.room).emit('message', {user: user.name, text: message});
-
+        io.to(user.room).emit('message', { user: user.name, text: message });
+    
         callback();
     });
 
     socket.on('disconnect', () => {
-        console.log('user disconnected from socketio');
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+          }
     });
 });
-
-app.use(router);
 
 server.listen(PORT, () => {
     console.log(`server has started on port ${PORT}`)
--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -2,27 +2,34 @@ import React, {useState, useEffect} from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import TextContainer from '../TextContainer/TextContainer';
 import './Chat.css';
+import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
 let socket;
 
 const Chat = ({location}) => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const endpoint = 'localhost:5000';
+    const ENDPOINT = 'http://localhost:5000/';
 
     useEffect(()=>{
         const {name, room} = queryString.parse(location.search);
 
-        socket = io(endpoint);
+        socket = io(ENDPOINT);
 
         setName(name);
         setRoom(room);
 
-        socket.emit('join', {name, room}, ()=>{
-
+        socket.emit('join', {name, room}, (error)=>{
+            if(error){
+                console.log('error has occured')
+            }
         });
 
         return () =>{
@@ -31,24 +38,40 @@ const Chat = ({location}) => {
             socket.off();
         }
 
-    }, [endpoint, location.search] );
+    }, [ENDPOINT, location.search] );
 
     useEffect(() => {
         socket.on('message', (message)=> {
             setMessages([...messages, message]);
         });
+
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+          })
+
+
     }, [messages]);
 
-    //function for sending messages
+const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message){
+        socket.emit('sendMessage', message, () => {
+            setMessage('');
+        })
+    }
+}
+
+console.log(message, messages);
 
     return (
         <div className="outerContainer">
             <div className="container">
-                <input value={message} 
-                onChange={(event) => setMessage(event.target.value)}
-                onKeyPress={(event) => event.key === 'Enter' ? sendMessage()} />
-                
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name}/>
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+            <TextContainer users={users}/>
         </div>
     )
 }
